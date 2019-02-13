8da40cb89c3390575c6c16cf65818a61c9c43664
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-02-12 21:20:43 -0800

    Update cplusplusInput.cpp

--- cplusplusInput.cpp
+++ cplusplusInput.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstrings>
+#include <cstring>
 using namespace std;
 
 struct PERSON{
@@ -10,42 +10,92 @@ struct PERSON{
 };
 
 void display(PERSON arr[], int n){
+  cout << "Name" << "       " << "Balance" << endl;
+  cout "------------------------------" << endl;
+  for(int 1 = 0; i < n; ++i){
+    cout << arr[i].name << " " << arr[i].balance << endl;
+  }
 
 }
 
 void findRichest(PERSON arr[], int n){
-
+  char richest[20];
+  for(int i = 0; i < n -1; ++i){
+    if(arr[i].balance > arr[i+i]){
+      strcpy(richest, arr[i]);
+    }else{ strcpy(richest, arr[i+1]);}
+  }
+  cout << "The customer with the highest balance is ";
+  cout << richest << endl;
 }
 
-void deposit(PERSON arr[], int n){
-
+void deposit(string name, PERSON arr[], int n){
+  float add;
+  int found;
+  for(int i = 0; i < n; ++i){
+    if(strcmp(name.c_str(), arr[i].name) == 0){
+      cout << name << ", how much would you like to deposit? ";
+      cin >> add;
+      cout << endl;
+      found = i;
+    }
+  }
+  add += arr[found].balance;
+  cout << "Now your balance is " << add << endl;
 }
 
-int readFile(string fileName){
-  int num;
+void readFile(string fileName, PERSON arr[], int n){
+  fstream myFile2;
+  myFile2.open(filename);
   string str1;
   string str2;
-  string line;
-  float value;
-  ifstream inData;
-  inData.open(fileName);
-  inData >> str1;
-  while (inData)
-    {
-      inData >> str1;
-      inData >> value;
-      getline (inData, line);
-      inData >> str1;
-      ++num;
-    }
-  return num;
+  string sName;
+  float pay;
+  string space;
+
+  for(int i = 0; i < n; ++i){
+    myFile2 >> str1 >> str2;
+    sName = str1 + " " + str2;
+    strcpy(arr[n].name, sName.c_str());
+    myFile2 >> pay;
+    arr[n].balance = pay;
+    getline(myFile2, space);
+  }
+  myFile.close();
 }
 
-int main(){
-  int count;
-  count = readFile("data.txt");
-  PERSON personArray[count];
-  cout << count;
+int numOfLines(string filename){
+  int count = 0;
+  string line;
+  fstream myFile;
+  myFile.open(fileName);
+  if(myFile.is_open()){
+    while(!myFile.eof()){
+      getline(myFile,line);
+      ++count;
+      }
+    myFile.close();
+  }
+  return count;
+}
 
+
+
+int main(){
+  int n;
+  string filename;
+  string name;
+  cout << "Enter the filename: " << endl; 
+  cin >> filename;
+  n = nomOfLines(filename);
+  
+  PERSON arr[n];
+  readFile("data.txt", arr, n);
+  display(arr, n);
+  findRichest(arr, n);
+  cout << "Enter your full name to deposite money: ";
+  cin >> name;
+  cout << endl;
+  
   return 0;
 }
